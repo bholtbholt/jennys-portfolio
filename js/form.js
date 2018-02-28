bd95362f7709a5617c7f1b3a48eda208e5a1d1b3
Brian Holt <bholtbholt@icloud.com>
2018-02-27 20:52:49 -0800

    Add honeypot and fade out

--- js/form.js
+++ js/form.js
@@ -11,9 +11,8 @@
 
   form.onsubmit = e => {
     e.preventDefault();
-
-    const formElements = Array.from(form);
-    const data = {};
+    // Escape if the honeypot has been filled
+    if (!!form.children.namedItem('honeypot').value) return;
 
     // Disable submissions, show loading
     const submitButton = e.target.querySelector('[type="submit"]');
@@ -21,8 +20,11 @@
 
     // reset form response message
     formResponse.innerHTML = '';
-    formResponse.classList.remove('_success', '_warning', '_slide-in');
+    formResponse.classList.remove('_success', '_warning', '_slide-in', '_fade-out');
 
+    // Prepare data to send
+    const data = {};
+    const formElements = Array.from(form);
     formElements.map(input => (data[input.name] = input.value));
 
     // Construct an HTTP request
@@ -38,8 +40,8 @@
     xhr.onloadend = response => {
       toggleButtonState(submitButton);
       if (response.target.status === 200) {
-        formElements.filter(input => input.type !== 'hidden').map(input => (input.value = ''));
-        formResponse.classList.add('_success', '_slide-in');
+        form.reset();
+        formResponse.classList.add('_success', '_slide-in', '_fade-out');
         formResponse.innerHTML = "Thanks for the message. I'll be in touch shortly.";
       } else {
         formResponse.classList.add('_warning', '_slide-in');
